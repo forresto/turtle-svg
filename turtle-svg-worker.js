84ed4b4db16031dd0121499bc8d97b62fa1b883b
Forrest Oliphant <forrest@sembiki.com>
2013-02-05 08:24:50 +0200

    direct SVG M m L l commands

--- turtle-svg-worker.js
+++ turtle-svg-worker.js
@@ -2,19 +2,21 @@
 var TAU = 2 * Math.PI;
 
 // Reset turtle
-var moveCount = 0;
-var pen = true;
-var d = "M 250 250 ";
-var vector = {
+var _moveCount = 0;
+var _pen = true;
+var _pathD = "M 250 250 ";
+var _vector = {
   x: 0,
   y: 1
 };
-var currentAngle = 0;  
+var _currentAngle = 0;  
+
+var _accuracy = 1000000000;
 
 // Relative turns, angles are 0.0 to 1.0
 var turn = function(){
-  vector.x = Math.sin(TAU*currentAngle);
-  vector.y = Math.cos(TAU*currentAngle);
+  _vector.x = Math.round( Math.sin(TAU*_currentAngle) * _accuracy ) / _accuracy;
+  _vector.y = Math.round( Math.cos(TAU*_currentAngle) * _accuracy ) / _accuracy;
 };
 
 var turnRight, r;
@@ -23,58 +25,74 @@ turnRight = r =  function(angle){
 };
 var turnLeft, l;
 turnLeft = l = function(angle){
-  currentAngle += angle;
-  currentAngle = currentAngle%1;
+  _currentAngle += angle;
+  _currentAngle = _currentAngle%1;
   turn();
 };
 
 // Absolute turn
 var turnTo, t;
 turnTo = t = function(angle){
-  currentAngle = angle;
+  _currentAngle = angle;
   turn();
 };
 
 // Drawing
 var penUp, u;
 penUp = u = function(){
-  pen = false;
+  _pen = false;
 };
 var penDown, d;
 penDown = d = function(){
-  pen = true;
+  _pen = true;
 };
 
 // Relative moves
 var moveForward, f;
 moveForward = f = function (distance) {
-  d += pen ? "l " : "m ";
-  d += (distance * vector.x) + " " + (distance * vector.y) + " ";
-  moveCount++;
+  _pathD += _pen ? "l " : "m ";
+  var x = Math.round( distance * _vector.x * _accuracy) / _accuracy;
+  var y = Math.round( distance * _vector.y * _accuracy) / _accuracy;
+  _pathD += x + " " + y + " ";
+  _moveCount++;
 }
 
-// Absolute moves
-var moveTo, m;
-moveTo = m = function (x, y) {
-  d += pen ? "L " : "M ";
-  d += x + " " + y + " ";
-  moveCount++;
+// SVG path 
+// Absolute move
+var Move = function (x, y) {
+  _pathD += "M " + x + " " + y + " ";
+  _moveCount++;
+}
+// Relative move
+var move = function (x, y) {
+  _pathD += "m " + x + " " + y + " ";
+  _moveCount++;
+}
+// Absolute line
+var Line = function (x, y) {
+  _pathD += "L " + x + " " + y + " ";
+  _moveCount++;
+}
+// Relative line
+var line = function (x, y) {
+  _pathD += "l " + x + " " + y + " ";
+  _moveCount++;
 }
 
 // Worker setup
 self.onmessage = function(e) {
 
   // Reset
-  moveCount = 0;
-  pen = true;
-  d = "M 250 250 ";
-  vector.x = 0;
-  vector.y = 1;
-  currentAngle = 0; 
+  _moveCount = 0;
+  _pen = true;
+  _pathD = "M 250 250 ";
+  _vector.x = 0;
+  _vector.y = 1;
+  _currentAngle = 0; 
 
   try {
     eval(e.data);
-    self.postMessage(d);
+    self.postMessage(_pathD);
   } catch (error) {
     // err
     self.postMessage("");
