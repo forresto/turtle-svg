1f60fd43f0426590dcd79a306c10996b30008f20
Forrest Oliphant <forrest@sembiki.com>
2013-02-06 11:08:37 +0200

    using ACE's jshint before eval; isNaN testing in turtle functions

--- turtle-svg-worker.js
+++ turtle-svg-worker.js
@@ -1,30 +1,38 @@
 // Turtle graphics drawing to SVG (?)
 var TAU = 2 * Math.PI;
+var _accuracy = 1000000000;
 
 // Reset turtle
-var _moveCount = 0;
-var _pen = true;
-var _pathD = "M 250 250 ";
-var _vector = {
-  x: 0,
-  y: 1
+var _moveCount, _pen, _pathD, _vector, _currentAngle;
+var resetTurtle = function(){
+  _moveCount = 0;
+  _pen = true;
+  _pathD = "M 250 250 ";
+  _vector = {
+    x: 0 * _accuracy,
+    y: 1 * _accuracy
+  };
+  _currentAngle = 0;  
 };
-var _currentAngle = 0;  
-
-var _accuracy = 1000000000;
+resetTurtle();
 
 // Relative turns, angles are 0.0 to 1.0
 var turn = function(){
-  _vector.x = Math.round( Math.sin(TAU*_currentAngle) * _accuracy ) / _accuracy;
-  _vector.y = Math.round( Math.cos(TAU*_currentAngle) * _accuracy ) / _accuracy;
+  // Using _accuracy because otherwise we get some stupid tiny numbers
+  _vector.x = Math.round( Math.sin(TAU*_currentAngle) * _accuracy );
+  _vector.y = Math.round( Math.cos(TAU*_currentAngle) * _accuracy );
 };
 
 var turnRight, r;
 turnRight = r =  function(angle){
+  if (isNaN(angle)) { return; }
+
   turnLeft(-angle);
 };
 var turnLeft, l;
 turnLeft = l = function(angle){
+  if (isNaN(angle)) { return; }
+
   _currentAngle += angle;
   _currentAngle = _currentAngle%1;
   turn();
@@ -33,6 +41,8 @@ turnLeft = l = function(angle){
 // Absolute turn
 var turnTo, t;
 turnTo = t = function(angle){
+  if (isNaN(angle)) { return; }
+
   _currentAngle = angle;
   turn();
 };
@@ -50,9 +60,11 @@ penDown = d = function(){
 // Relative moves
 var moveForward, f;
 moveForward = f = function (distance) {
+  if (isNaN(distance)) { return; }
+
   _pathD += _pen ? "l " : "m ";
-  var x = Math.round( distance * _vector.x * _accuracy) / _accuracy;
-  var y = Math.round( distance * _vector.y * _accuracy) / _accuracy;
+  var x = distance * _vector.x / _accuracy;
+  var y = distance * _vector.y / _accuracy;
   _pathD += x + " " + y + " ";
   _moveCount++;
 }
@@ -60,21 +72,29 @@ moveForward = f = function (distance) {
 // SVG path 
 // Absolute move
 var moveTo = function (x, y) {
+  if (isNaN(x) || isNaN(y)) { return; }
+
   _pathD += "M " + x + " " + y + " ";
   _moveCount++;
 }
 // Relative move
 var moveBy = function (x, y) {
+  if (isNaN(x) || isNaN(y)) { return; }
+
   _pathD += "m " + x + " " + y + " ";
   _moveCount++;
 }
 // Absolute line
 var lineTo = function (x, y) {
+  if (isNaN(x) || isNaN(y)) { return; }
+
   _pathD += "L " + x + " " + y + " ";
   _moveCount++;
 }
 // Relative line
 var lineBy = function (x, y) {
+  if (isNaN(x) || isNaN(y)) { return; }
+
   _pathD += "l " + x + " " + y + " ";
   _moveCount++;
 }
@@ -82,17 +102,15 @@ var lineBy = function (x, y) {
 // Worker setup
 self.onmessage = function(e) {
 
-  // Reset
-  _moveCount = 0;
-  _pen = true;
-  _pathD = "M 250 250 ";
-  _vector.x = 0;
-  _vector.y = 1;
-  _currentAngle = 0; 
+  resetTurtle();
 
   try {
     eval(e.data);
-    self.postMessage(_pathD);
+
+    self.postMessage({
+      path: _pathD,
+      code: e.data
+    });
   } catch (error) {
     // err
     self.postMessage("");
---
+++ turtle-svg.js
@@ -0,0 +1,198 @@
+window.onload = function(){
+
+  ace.config.set("workerPath", ".");
+  var editor = ace.edit("editor");
+  editor.setTheme("ace/theme/monokai");
+  var session = editor.getSession();
+  session.setMode("ace/mode/javascript");
+  session.setTabSize(2);
+
+  var applyButton = document.getElementById("apply");
+  var autoCheck = document.getElementById("auto");
+  var helpButton = document.getElementById("show-help");
+  var exportButton = document.getElementById("export");
+  var rightColumn = document.getElementById("right-column");
+  var link = document.getElementById("link");
+  var help = document.getElementById("help");
+  var svg = document.getElementById("svgOutput");
+  var path = svg.getElementById("turtle");
+
+  // var changeTimeout;
+  var testCodeStart;
+  var testCodeWorking = true;
+  var testingCode;
+
+  var currentSVGCode = "";
+
+  var autoEval = true;
+
+  var worker;
+  var workerBusy = false;
+  var workerError = false;
+
+  var setupWorker = function(){
+    worker = new Worker('turtle-svg-worker.js');
+    worker.onmessage = function(e) {
+      if (e.data === ""){
+        workerError = true;
+        applyButton.innerHTML = "JS error... restart?";
+        applyButton.disabled = false;
+      } else {
+        workerError = false;
+        applyButton.innerHTML = "Apply";
+        applyButton.disabled = autoEval;
+        setPath(e.data);
+      }
+      workerBusy = false;
+    };
+    worker.onerror = function(e) {
+      console.log("worker error", e);
+    };
+    workerError = false;
+    workerBusy = false;
+  }
+  setupWorker();
+
+  var testCode = function(){
+    if (worker && !workerBusy) {
+      workerBusy = true;
+      applyButton.innerHTML = "Working... cancel?";
+      applyButton.disabled = false;
+      testCodeStart = Date.now();
+      testingCode = editor.getValue();
+      worker.postMessage(testingCode);      
+    } else {
+      // ?
+    }
+  };
+
+  autoCheck.onchange = function(e){
+    autoEval = autoCheck.checked;
+    applyButton.disabled = autoEval;
+    if (autoEval) {
+      testCode();
+    }
+  };
+
+  applyButton.onclick = function(){
+    // Restart
+    if (worker && (workerBusy || workerError)) {
+      worker.terminate();
+      setupWorker();
+      applyButton.innerHTML = "Apply";
+
+      autoCheck.checked = false;
+      autoCheck.onchange();
+    } else {
+      testCode();
+    }
+  };
+
+  // session.on("change", function (e) {
+  //   if (!autoEval){
+  //     return;
+  //   }
+  //   if(changeTimeout) {
+  //     clearTimeout(changeTimeout);
+  //   }
+  //   changeTimeout = setTimeout(testCode, 500);
+  // });
+
+
+  session.on("changeAnnotation", function(){
+    var annotations = editor.getSession().getAnnotations();
+    var jshintOK = true;
+    for (key in annotations) {
+      if (annotations.hasOwnProperty(key)){
+        jshintOK = false;
+      }
+    }
+
+    // Eval it?
+    if (jshintOK) {
+      applyButton.innerHTML = "Apply";
+      if (autoEval) {
+        testCode();
+      } else {
+        applyButton.disabled = false;
+      }
+    } else {
+      applyButton.innerHTML = "Check your code.";
+      if (!autoEval){
+        applyButton.disabled = true;
+      }
+    }
+  });
+
+  var setPath = function(message){
+    var d = message.path;
+    currentSVGCode = message.code;
+
+    path.setAttributeNS(null, "d", d);
+
+    var bBox = path.getBBox();
+    var w = Math.max(500, Math.ceil(bBox.x+bBox.width+20));
+    var h = Math.max(500, Math.ceil(bBox.y+bBox.height+20));
+    svg.setAttribute("width", w);
+    svg.setAttribute("height", h);
+  };
+
+
+  var helpShown = false;
+  helpButton.onclick = function(){
+    helpShown = !helpShown;
+    help.style.display = helpShown ? "block" : "none";
+    rightColumn.scrollTop = helpShown ? help.offsetTop : 0;
+  };
+
+  window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL || null;
+  exportButton.onclick = function(){
+    var packed = encode( currentSVGCode );
+    var perma = 'http://forresto.github.com/turtle-svg/#code/' + packed;
+    var comment = "<!-- \n\nMade with LASER TURTLE; here is editor and source: \n" + perma + "\n\n-->\n";
+    var svgString = document.getElementById("svgContainer").innerHTML;
+    var svgBlob = new Blob([comment,svgString], { "type" : "image/svg+xml" });
+    var svgBlobURL = URL.createObjectURL(svgBlob);
+    if (svgBlobURL) {
+      window.open(svgBlobURL);
+      // window.URL.revokeObjectURL(svgBlobURL); 
+    } else {
+      window.open("data:image/svg+xml,"+svgString);
+    }
+  }
+
+  // Compress code for sharing
+  link.onclick = function(){
+    // With help from https://github.com/mrdoob/htmleditor
+    var packed = encode( editor.getValue() );
+    var perma = 'http://forresto.github.com/turtle-svg/#code/' + packed;
+    // window.location.replace("#code/"+packed); 
+    window.location.href = "#code/"+packed;
+  }
+  var decode = function ( string ) {
+    return RawDeflate.inflate( window.atob( string ) );
+  };
+  var encode = function ( string ) {
+    return window.btoa( RawDeflate.deflate( string ) );
+  };
+
+  var loadCodeFromHash = function(e){
+    if (window.location.hash.substr(0,5) === "#code") {
+      try {
+        var code = decode( window.location.hash.substr(6) );
+        editor.setValue(code, 1);
+        testCode();
+      } catch (e) {}
+    }
+  }
+  window.onhashchange = loadCodeFromHash;
+
+  // See if code is set on load
+  if ( window.location.hash ) {
+    loadCodeFromHash();
+  } else {
+    // Default code
+    testCode();
+  }
+
+}
